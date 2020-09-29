fa52ad32fbe1fe34818cd66261d2b332c6b90ed5
CoralineK <k.kozakiewicz.13@gmail.com>
2020-09-29 13:01:54 +0200

    configuration

---
+++ services/firebase.tsx
@@ -0,0 +1,27 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyDJBa-KUc1LDNA1TaupoFFcXSQzP54QJYs',
+  authDomain: 'planner-85d57.firebaseapp.com',
+  databaseURL: 'https://planner-85d57.firebaseio.com',
+  projectId: 'planner-85d57',
+  storageBucket: 'planner-85d57.appspot.com',
+  messagingSenderId: '435077168151',
+  appId: '1:435077168151:web:f2f60bfb71d1e5cfd83052',
+  measurementId: 'G-3CZP11V1ET',
+};
+
+app.apps.length === 0 && app.initializeApp(config);
+
+export function createAccount(email, password) {
+  return app.auth().createUserWithEmailAndPassword(email, password);
+}
+
+export function signInUser(email, password) {
+  return app.auth().signInWithEmailAndPassword(email, password);
+}
+
+export function signOut() {
+  return app.auth().signOut();
+}
---
+++ src/style/Global.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
+
+export default function Global() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
